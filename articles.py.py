6e6ec7e69f7b425f649725ae1159a510bfaa313e
OznaDev <oznadev@gmail.com>
2024-11-15 10:47:06 +0100

    Delete articles.py.py

--- articles.py.py
+++
@@ -1,9 +0,0 @@
-productes = {
-    "Model X": {"especificacions": "Pantalla OLED, 128GB, Càmera 12MP", "preu": 699.99},
-    "Model Y": {"especificacions": "Pantalla LCD, 64GB, Càmera 8MP", "preu": 499.99},
-    "Model Z": {"especificacions": "Pantalla AMOLED, 256GB, Càmera 16MP", "preu": 899.99},
-    "Model A": {"especificacions": "Pantalla OLED, 256GB, Càmera 20MP", "preu": 1099.99},
-    "Model B": {"especificacions": "Pantalla Retina, 64GB, Càmera 12MP", "preu": 649.99},
-    "Model C": {"especificacions": "Pantalla Super LCD, 128GB, Càmera 10MP", "preu": 549.99},
-    "Model D": {"especificacions": "Pantalla IPS, 32GB, Càmera 8MP", "preu": 299.99}
-}
